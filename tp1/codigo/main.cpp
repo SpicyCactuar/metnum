a79240d4186fb9ca48809e290bc376a3705fe542
firstName lastName <yourEmail@wolox.com.ar>
2016-03-19 04:39:23 -0300

    empece con gauss, hay algo que no me cierra, da numeros negativos...

---
+++ tp1/codigo/main.cpp
@@ -0,0 +1,65 @@
+//COMPILAR! g++ -o main main.cpp -std=c++11
+#include "main.h"
+
+int main(int argc, char const *argv[]){
+	// si no me pasan los 4 parametros (argc es implicito) esta todo mal
+	if(argc != 4){
+		cout << "Falta algun parametro, son: input file, output file, algoritmo (0: CMM-EG, 1: CMM-CL, 2:WP)" << endl;
+		return 0;
+	}
+
+	// leo el archivo de entrada
+	ifstream input(argv[1]);
+	int n, k, local, local_score, away, away_score;
+	string temp;
+	input >> n >> k;
+	// ganados perdidos
+	vector<pair<double, double> > winAndLose(n, pair<double, double>(0., 0.));
+	matrix<double> scores(n, vector<double>(n, 0.));
+	for(int i = 0; i < k; i++){
+		input >> temp >> local >> local_score >> away >> away_score;
+		local -= 1;
+		away -= 1;
+		if(local_score > away_score){
+			winAndLose[local].first += 1;
+			winAndLose[away].second += 1;
+		} else {
+			winAndLose[local].second += 1;
+			winAndLose[away].first += 1;
+		}
+		scores[local][away] += 1;
+		scores[away][local] += 1;
+	}
+	input.close();
+
+	// armo la C
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < n; ++j){
+			if(j==i)
+				scores[i][j] += 2;
+			else
+				scores[i][j] *= -1;
+		}
+	}
+
+	// armo el b
+	vector<pair<double, int> >b_vector(n);
+	for (int i = 0; i < n; ++i)
+		b_vector[i] = pair<double, int>(1 + (winAndLose[i].first - winAndLose[i].second) / 2, i);
+
+	string method = argv[3];
+	if(method == "0"){
+		vector<pair<double, int> > res = eliminacionGaussiana(scores, b_vector);
+		ofstream output(argv[2]);
+		sort(res.begin(), res.end(), pairCompare);
+		printFile(res, output);
+		output.close();
+	} else if(method == "1"){
+		cout << "CMM-CL" << endl;
+	} else	if(method == "2"){
+		cout << "WP" << endl;
+	} else {
+		cout << "Cualquiera" << endl;
+	}
+	return 0;
+}
---
+++ tp1/codigo/main.h
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+using matrix = vector<vector<T> >;
+
+template<typename T, typename Q>
+ostream& operator<<(ostream& os, const pair<T, Q>& p ){
+	return os << "(" << p.first << " " << p.second << ")";
+}
+
+bool pairCompare(const pair<double, int>& first, const pair<double, int>& second) {
+	return first.first > second.first;
+}
+
+template<typename T>
+void printVector(const vector<T>& vec){
+	for (int i = 0; i < vec.size(); ++i)
+		cout << vec[i] << " ";
+	cout << endl;
+}
+
+void printFile(const vector<pair<double, int> >& vec, ostream& os){
+	for (int i = 0; i < vec.size(); ++i){
+		os << vec[i].second << " " << vec[i].first;
+		os << endl;
+	}
+}
+
+template<typename T>
+void printMatrix(const matrix<T>& mat){
+	for (int i = 0; i < mat.size(); ++i)
+		for (int j = 0; j < mat[i].size(); ++j)
+			cout << mat[i][j] << " ";
+		cout << endl;
+}
+
+template<typename T>
+int find_max(const matrix<T>& mat, int actual){
+	int max = actual;
+	for (int i = actual+1; i < mat.size(); ++i)
+		if(abs(mat[actual][i]) > abs(mat[actual][actual]))
+			max = i;
+	return max;
+}
+
+template<typename T, typename Q>
+vector<pair<double, int> > eliminacionGaussiana(matrix<T>& mat, vector<Q>& vec){
+	for (int i = 0; i < mat.size(); ++i){
+		// busco fila con el mayor elemento
+		int i_max = find_max(mat, i);
+		// si tiene 0 avanzo (creo que no es necesario por como es la matriz, pero nunca esta demas)
+		// si algun equipo no juega, tal vez se chotee feo
+		if(mat[i_max][i] != 0){
+			// intercambio la columna actual por la del maximo, en el b tambien (es una columna mas de C)
+			swap(mat[i_max], mat[i]);
+			swap(vec[i_max], vec[i]);
+			for (int j = i+1; j < mat.size(); ++j){
+				// magia para generar la matriz triangular superior
+				double c = mat[j][i]/mat[i][i];
+				mat[j][i] = 0;
+				for (int k = i+1; k < mat.size(); ++k)
+					mat[j][k] -= c * mat[i][k];
+				vec[j].first -= c * vec[i].first;
+			}
+		}
+	}
+	// resuelvo el sistema triangular superior
+	vector<pair<double, int> > res(mat.size());
+	for (int i = mat.size()-1; i >= 0; --i){
+		double partial_sum = 0.;
+		for (int j = i+1; j < mat.size(); ++j)
+			partial_sum += mat[i][j] * res[j].first;
+		res[i] = pair<double, int>((vec[i].first - partial_sum) / mat[i][i], vec[i].second);
+	}
+	return res;
+}
