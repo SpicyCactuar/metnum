215d68256174d9d243909d7ecc5e1279495d7695
Lucas Cortés <lucascortes@me.com>
2016-03-25 18:27:04 -0300

    make it build in all compilers. Actually, make it build

--- tp1/codigo/main.cpp
+++ tp1/codigo/main.cpp
@@ -2,68 +2,68 @@
 #include "main.h"
 
 int main(int argc, char const *argv[]){
-	// si no me pasan los 4 parametros (argc es implicito) esta todo mal
-	if(argc != 4){
-		cout << "Falta algun parametro, son: input file, output file, algoritmo (0: CMM-EG, 1: CMM-CL, 2:WP)" << endl;
-		return 0;
-	}
+    // si no me pasan los 4 parametros (argc es implicito) esta todo mal
+    if(argc != 4){
+        cout << "Falta algun parametro, son: input file, output file, algoritmo (0: CMM-EG, 1: CMM-CL, 2:WP)" << endl;
+        return 0;
+    }
 
-	// leo el archivo de entrada
-	ifstream input(argv[1]);
-	int n, k, local, local_score, away, away_score;
-	string temp;
-	input >> n >> k;
-	// ganados perdidos
-	vector<pair<double, double> > winAndLose(n, pair<double, double>(0., 0.));
-	matrix<double> scores(n, vector<double>(n, 0.));
-	for(int i = 0; i < k; i++){
-		input >> temp >> local >> local_score >> away >> away_score;
-		local -= 1;
-		away -= 1;
-		if(local_score > away_score){
-			winAndLose[local].first += 1;
-			winAndLose[away].second += 1;
-		} else {
-			winAndLose[local].second += 1;
-			winAndLose[away].first += 1;
-		}
-		scores[local][away] -= 1;
-		scores[away][local] -= 1;
-	}
-	input.close();
+    // leo el archivo de entrada
+    ifstream input(argv[1]);
+    int n, k, local, local_score, away, away_score;
+    string temp;
+    input >> n >> k;
+    // ganados perdidos
+    vector<pair<double, double> > winAndLose(n, pair<double, double>(0., 0.));
+    matrix<double> scores(n, vector<double>(n, 0.));
+    for(int i = 0; i < k; i++){
+        input >> temp >> local >> local_score >> away >> away_score;
+        local -= 1;
+        away -= 1;
+        if(local_score > away_score){
+            winAndLose[local].first += 1;
+            winAndLose[away].second += 1;
+        } else {
+            winAndLose[local].second += 1;
+            winAndLose[away].first += 1;
+        }
+        scores[local][away] -= 1;
+        scores[away][local] -= 1;
+    }
+    input.close();
 
-	// armo la C
-	for (int i = 0; i < n; ++i){
-		scores[i][i] += winAndLose[i].first + winAndLose[i].second + 2;
-	}
+    // armo la C
+    for (int i = 0; i < n; ++i){
+        scores[i][i] += winAndLose[i].first + winAndLose[i].second + 2;
+    }
 
-	// armo el b
-	vector<pair<double, int> >b_vector(n);
-	for (int i = 0; i < n; ++i)
-		b_vector[i] = pair<double, int>(1 + (winAndLose[i].first - winAndLose[i].second) / 2, i);
+    // armo el b
+    vector<pair<double, int> >b_vector(n);
+    for (int i = 0; i < n; ++i)
+        b_vector[i] = pair<double, int>(1 + (winAndLose[i].first - winAndLose[i].second) / 2, i);
 
-	string method = argv[3];
-	if(method == "0"){
-		vector<pair<double, int> > res(n, pair<double, int>(0., 0));
-		gaussianElimination(scores, b_vector, res);
-		ofstream output(argv[2]);
-		sort(res.begin(), res.end(), pairCompare);
-		printFile(res, output);
-		output.close();
-	} else if(method == "1"){
-		vector<pair<double, int> > res(n, pair<double, int>(0., 0));
-		choleskyFactorization(scores, b_vector, res);
-		ofstream output(argv[2]);
-		sort(res.begin(), res.end(), pairCompare);
-		printFile(res, output);
-		output.close();
-	} else	if(method == "2"){
-		vector<pair<double, int> > res(n, pair<double, int>(0., 0));
-		ofstream output(argv[2]);
-		winningPercentage(winAndLose, res);
-		printFile(res, output);
-	} else {
-		cout << "Cualquiera" << endl;
-	}
-	return 0;
+    string method = argv[3];
+    if(method == "0"){
+        vector<pair<double, int> > res(n, pair<double, int>(0., 0));
+        gaussianElimination(scores, b_vector, res);
+        ofstream output(argv[2]);
+        sort(res.begin(), res.end(), pairCompare);
+        printFile(res, output);
+        output.close();
+    } else if(method == "1"){
+        vector<pair<double, int> > res(n, pair<double, int>(0., 0));
+        choleskyFactorization(scores, b_vector, res);
+        ofstream output(argv[2]);
+        sort(res.begin(), res.end(), pairCompare);
+        printFile(res, output);
+        output.close();
+    } else	if(method == "2"){
+        vector<pair<double, int> > res(n, pair<double, int>(0., 0));
+        ofstream output(argv[2]);
+        winningPercentage(winAndLose, res);
+        printFile(res, output);
+    } else {
+        cout << "Cualquiera" << endl;
+    }
+    return 0;
 }
